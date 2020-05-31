1a2d433860ac936cbcb7bee15cc05616a3b1c8ab
Szymon <wawakszymon@gmail.com>
2020-05-31 17:41:30 +0200

    Added clouds that generate in loop

--- src/index.js
+++ src/index.js
@@ -24,13 +24,24 @@ const game = new Phaser.Game(config);
 function preload() {
     this.load.image('hamster', 'src/assets/hamster.png');
     this.load.image('sky', 'src/assets/sky.png');
+    this.load.image('cloud', 'src/assets/cloud.png')
 }
 
 function create() {
     let context = this;
-    this.add.image(512, 384, 'sky');
+    let sceneSize = this.scale.displaySize;
+    this.add.image(sceneSize.width / 2, sceneSize.height / 2, 'sky');
     this.hamster = this.physics.add.sprite(100, 245, 'hamster');
     this.input.keyboard.on('keydown_SPACE', function () {
         Actions.jump(context);
     });
+    this.clouds = this.physics.add.group();
+    this.time.addEvent({delay: 1500, callback: addCloud, callbackScope: this, loop: true})
+}
+
+function addCloud() {
+    let height = Math.random() * 1024;
+    let cloud = this.clouds.create(this.game.config.width, height, 'cloud');
+    cloud.body.velocity.x = -200;
+    cloud.body.allowGravity = false;
 }
